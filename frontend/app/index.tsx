1b464a72be88ffbdb6a45a93b9797e77d7b121aa
ashley <aiattoni@berkeley.edu>
2026-04-21 20:29:55 -0700

    DogSense Sign In Page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TextInput, Pressable, Image} from "react-native";
 
 export default function Index() {
+  const [input, setInput] = useState("");
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        backgroundColor: "#f5f5f5",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        paddingTop: 80,
+        paddingHorizontal: 30,
+      }}>
+      <Text style={{ 
+        fontSize: 20,
+        fontWeight: "600",
+        marginBottom: 40,
+        color: "#000",}}>SIGN IN</Text>
+      
+      <Image
+        source={require("../assets/images/Dailysense_logo.png")}
+        style={{
+          width: 300,
+          height: 210,
+          marginBottom: 20,}}
+        resizeMode="contain"/>
+
+      <View style={{
+        flexDirection: "row",
+        marginBottom: 35,}}>
+        <Text style={{
+          fontSize: 48,
+          fontWeight: "600",
+          color: "#c8102e",}}>Dog</Text>
+        <Text style={{
+          fontSize: 48,
+          fontWeight: "600",
+          color: "#1f5fbf",}}>Sense</Text>
+        </View>
     </View>
   );
 }
