7896c2e53dc81959b48f5c6f7aeef04c5453172c
Yonatan <yonatan.herrera001@gmail.com>
2026-04-21 20:38:21 -0700

    new packages

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ fontSize: 12 }}>Edit the repo to edit this screen.</Text>
     </View>
   );
 }
